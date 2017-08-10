388691e4c966bba4c266f923e297abc3f26c362d
manoj bojja <manojbojja@MacBook-Pro.local>
2017-08-11 02:23:41 +0530

    first commit

---
+++ app/src/main/java/com/example/manojbojja/notificationtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.manojbojja.notificationtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.carnival.sdk.Carnival;
+import com.carnival.sdk.NotificationConfig;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Carnival.startEngine(getApplicationContext(), "60c5c679faf4cb5ff2f80f408ad778ac290953de");
+
+        NotificationConfig config = new NotificationConfig();
+        config.setSmallIcon(R.drawable.ic_stat_notification);
+
+        Carnival.setNotificationConfig(config);
+    }
+}
---
+++ app/src/main/java/com/example/manojbojja/notificationtest/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.manojbojja.notificationtest;
+
+import android.app.Application;
+
+/**
+ * Created by manojbojja on 03/08/17.
+ */
+
+public class MyApplication extends Application {
+
+
+}
